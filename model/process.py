ae6904a40833e558dbf737dc3f131430ea263c11
JalexCode <javierglez9910@gmail.com>
2022-06-03 10:53:28 -0400

    Added docs get_file() was splitted in request_file() to get last modification date and download_file() to download the html file in Stream mode Refactoring

--- model/process.py
+++ model/process.py
@@ -1,13 +1,7 @@
 from datetime import datetime
-
+from bs4 import BeautifulSoup
 import requests
-from treelib import Tree, Node
-from bs4 import BeautifulSoup, Tag
-import requests
-from model.exceptions import BadServerResponseException
-from model.file_node import FileNode
 from contextlib import closing
-from util.const import *
 from util.util import *
 import time
 
@@ -21,16 +15,38 @@ class Request:
         self.error_signal = error
         self.finish_signal = finish
 
-    def get_file(self):
+    def request_file(self):
+        '''
+        Gets listado.html 'Last-Modified' header
+        :return:
+        '''
         try:
+            self.info_signal.emit("Solicitando datos remotos...")
+            with closing(requests.get(LISTADO_HTML_FILE, verify=False)) as response:
+                if response.status_code == 200:
+                    #
+                    self.info_signal.emit("Datos remotos obtenidos")
+                    self.finish_signal.emit(response.headers['Last-Modified'])
+                else:
+                    raise BadServerResponseException(response.status_code)
+        except Exception as error:
+            self.error_signal.emit(error)
+
+    def download_file(self):
+        '''
+        Download listado.html file in stream mode
+        :return:
+        '''
+        try:
+            self.info_signal.emit("Solicitando datos remotos...")
             with closing(requests.get(LISTADO_HTML_FILE, verify=False, timeout=TIMEOUT, stream=True)) as response:
                 if response.status_code == 200:
-                    self.info_signal.emit("Limpiando archivo...")
+                    self.info_signal.emit(f"Inicializando archivo '{DIRS_FILE_NAME}'...")
                     #
                     with open(DIR_FILE, "w", encoding="utf-8") as listado_file:
                         listado_file.write("")
                     #
-                    self.info_signal.emit("Descargando archivo...")
+                    self.info_signal.emit("Descargando...")
                     # file size...in bytes
                     file_size = len(response.content)
                     # file data that has been downloaded [in bytes]
@@ -52,39 +68,45 @@ class Request:
                             left_time = (file_size - downloaded) / speed
                             # emit progress signal
                             self.progress_signal.emit(percent, speed, left_time)
-                        print("[OK] FILE WAS SAVED")
-                        # finish
-                        self.info_signal.emit(f"Archivo guardado en {DIRS_FILE_NAME}")
+                    print("[OK] FILE WAS SAVED")
+                    # finish
+                    self.info_signal.emit(f"Archivo guardado en '{DIRS_FILE_NAME}'")
+                    self.finish_signal.emit(True)
                 else:
                     raise BadServerResponseException(response.status_code)
-            #
-            self.finish_signal()
         except Exception as error:
-            print(str(error.args))
             self.error_signal.emit(error)
 
     def get_page(self, url: str, parent: str = ""):
+        '''
+        Get all files metadata in the given page
+        :param url:
+        :param parent:
+        :return:
+        '''
         self.info_signal.emit("Solicitando datos remotos...")
         files = []
-        with closing(requests.get(url, verify=False, timeout=TIMEOUT)) as response:
-            if response.status_code == 200:
-                self.info_signal.emit("Leyendo datos...")
-                # paarse data
-                bs = BeautifulSoup(response.content)
-                children = bs.find_all("td")[5:]
-                for i in range(0, len(children), 5):
-                    size = children[i + 3].get_text().strip()
-                    # if isn't a folder (then is a file, obviusly ;-;)
-                    if size != "-":
-                        # get files metadata
-                        size = get_bytes(size)
-                        type = get_type(children[i].find("img").attrs['src'])
-                        filename = children[i + 1].get_text().strip()
-                        modificated_date = children[i + 2].get_text().strip()
-                        modificated_date = datetime.fromisoformat(modificated_date)
-                        href = parent + children[i + 1].find("a").attrs['href']
-                        print(href)
-                        files.append(FileNode(filename, modificated_date, size, href, type))
-                        # emit progress signal
-                        self.progress_signal.emit(i * 100 // len(children) * 5, None, None)
-        self.finish_signal.emit(files)
+        try:
+            with closing(requests.get(url, verify=False, timeout=TIMEOUT)) as response:
+                if response.status_code == 200:
+                    self.info_signal.emit("Leyendo datos...")
+                    # paarse data
+                    bs = BeautifulSoup(response.text)
+                    children = bs.find_all("td")[5:]
+                    for i in range(0, len(children), 5):
+                        size = children[i + 3].get_text().strip()
+                        # if isn't a folder (then is a file, obviusly ;-;)
+                        if size != "-":
+                            # get files metadata
+                            size = get_bytes(size)
+                            type = get_type(children[i].find("img").attrs['src'])
+                            filename = children[i + 1].get_text().strip()
+                            modificated_date = children[i + 2].get_text().strip()
+                            modificated_date = datetime.fromisoformat(modificated_date)
+                            href = parent + children[i + 1].find("a").attrs['href']
+                            files.append(FileNode(filename, modificated_date, size, href, type))
+                            # emit progress signal
+                            self.progress_signal.emit(i * 100 // len(children) * 5, None, None)
+            self.finish_signal.emit(files)
+        except Exception as error:
+            self.error_signal.emit(error)
