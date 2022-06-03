16294437259d4f174edcc93fbd9312bf915fb015
JalexCode <javierglez9910@gmail.com>
2022-06-03 10:44:52 -0400

    initial commit

--- model/threads.py
+++ model/threads.py
@@ -1,15 +1,24 @@
 from PyQt5.QtCore import QObject, pyqtSignal
-
-from login import Login
-
-class LoginThread(QObject):
-    post_request_signal = pyqtSignal()
+from model.process import Request
+class RequestThread(QObject):
+    info_signal = pyqtSignal(object)
+    progress_signal = pyqtSignal(object, object, object)
     error_signal = pyqtSignal(object)
-    finish_signal = pyqtSignal()
-    def __init__(self, login_object:Login):
+    finish_signal = pyqtSignal(object)
+    def __init__(self):
         QObject.__init__(self)
-        self.login_object:Login = login_object
 
-    def run(self):
-        self.login_object.signals(self.post_request_signal, self.error_signal, self.finish_signal)
-        self.login_object.post_data()
+    def request_file(self):
+        request = Request()
+        request.signals(self.info_signal, self.progress_signal, self.error_signal, self.finish_signal)
+        request.request_file()
+
+    def download_file(self):
+        request = Request()
+        request.signals(self.info_signal, self.progress_signal, self.error_signal, self.finish_signal)
+        request.download_file()
+
+    def get_page(self, url:str, parent:str):
+        request = Request()
+        request.signals(self.info_signal, self.progress_signal, self.error_signal, self.finish_signal)
+        request.get_page(url=url, parent=parent)
