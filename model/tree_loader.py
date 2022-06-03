0b767084f02bc7e0bfbced85b0a51ac5de2defac
JalexCode <javierglez9910@gmail.com>
2022-06-03 13:44:35 -0400

    + progress_signal + set BeautifulSoup features to "lxml"

--- model/tree_loader.py
+++ model/tree_loader.py
@@ -1,9 +1,9 @@
 from treelib import Tree
 from bs4 import BeautifulSoup
 from bs4.element import Tag
-
+import time
 from model.folder_node import FolderNode
-
+#import pdb
 
 def __get_parent(element: Tag) -> str:
     href = element.attrs['href']
@@ -26,18 +26,34 @@ def __get_root_name_and_identifier(link_tags: list) -> tuple:
     return dir_name, dir_identifier
 
 
-def load_visuales_tree(html_string: str) -> Tree:
-    soup = BeautifulSoup(html_string)
+def load_visuales_tree(html_string: str, progress_signal=None) -> Tree:
+    #pdb.set_trace()
+    soup = BeautifulSoup(markup=html_string, features="lxml")
     link_tags = soup.find_all(name='a')
-
+    #
     tree = Tree()
     # Root------------
     dir_name, dir_identifier = __get_root_name_and_identifier(link_tags)
     tree.create_node(tag=FolderNode(dir_name, False, False), identifier=dir_identifier)
     # ----------------
-    for html_tag in link_tags[1:]:
+    t0 = time.time()
+    tags = link_tags[1:]
+    for i in range(len(tags)):
+        html_tag = tags[i]
         dir_name = __get_name_of_directory(html_tag)
         dir_identifier = __get_identifier_of_directory(html_tag)
         dir_parent = __get_parent(html_tag)
         tree.create_node(tag=FolderNode(dir_name, False, False), identifier=dir_identifier, parent=dir_parent)
+        #
+        if progress_signal is not None:
+            i += 1
+            delay = time.time() - t0
+            # calculate percent
+            percent = i * 100 // len(tags) + 1
+            # speed
+            speed = int(i // (delay + 1))
+            # left_time
+            left_time = ((len(tags) + 1) - i) / speed
+            # emit
+            progress_signal.emit(percent, speed, left_time)
     return tree
